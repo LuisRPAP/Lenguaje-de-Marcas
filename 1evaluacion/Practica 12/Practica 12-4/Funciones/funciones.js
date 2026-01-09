9e0321499d350e192c9d83739581d96c683d13b9
LRP <lufer5775@gmail.com>
2026-01-09 21:08:25 +0100

    primera subida

---
+++ 1evaluacion/Practica 12/Practica 12-4/Funciones/funciones.js
@@ -0,0 +1,6 @@
+function cambiaColor(id_objeto){
+    document.getElementById(id_objeto).style.backgroundColor="red";
+}
+function regresaColor(id_objeto){
+    document.getElementById(id_objeto).style.backgroundColor="red";
+}
