4046001036d2348936d458185f434a3d77048cf9
LRP <lufer5775@gmail.com>
2026-01-16 21:42:38 +0100

    Ejercicio 12.5 a medias pero vamos bien

--- 1evaluacion/Practica 12/Practica 12-5/Funciones/funciones.js
+++ 1evaluacion/Practica 12/Practica 12-5/Funciones/funciones.js
@@ -5,27 +5,12 @@
             function sinfoco(id_objeto) {
                 document.getElementById(id_objeto).style.border="2px solid black";
             }
-            function colorAnt(id_objeto) {
-                document.getElementById(id_objeto).style.backgroundColor="blue";
+            
+            function focozona(id_objeto) {
+                document.getElementById(id_objeto).style.backgroundColor="rgba(155, 155, 155, 0.5)";
+                
             }
-            function gracias(id_objeto) {
-            document.getElementById(id_objeto).innerHTML = " Mmmmm Gracias";
+            function sinfocozona(id_objeto) {
+                document.getElementById(id_objeto).style.backgroundColor="transparent";
+                
             }
-            function raton(id_objeto) {
-            document.getElementById(id_objeto).innerHTML = "Pasa el ratón";
-            }
-            function bombillaOn(id_objeto) {
-                document.getElementById(id_objeto).src="Imagenes/BombillaEncendida.png";
-            }
-            function bombillaOff(id_objeto) {
-                document.getElementById(id_objeto).src="Imagenes/BombillaApagada.png";
-            }
-            function posicion1(id_objeto) {
-                document.getElementById("b5").style.visibility = "hidden";
-                document.getElementById("b6").style.visibility = "visible";
-            }
-            function posicion2(id_objeto) {
-                document.getElementById("b5").style.visibility ="visible";
-                document.getElementById("b6").style.visibility ="hidden";
-            }
-        
