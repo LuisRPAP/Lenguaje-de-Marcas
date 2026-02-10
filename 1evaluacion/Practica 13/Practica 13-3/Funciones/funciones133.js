6c8b0227575cc1afac7b2c2359ab1807dc71872c
LRP <lufer5775@gmail.com>
2026-02-10 17:51:40 +0100

    Organizando lenguaje de marcas

---
+++ 1evaluacion/Practica 13/Practica 13-3/Funciones/funciones133.js
@@ -0,0 +1,7 @@
+
+            function cocheOn(id_objeto){
+    document.getElementById(id_objeto).style.width="30%";
+            }
+            function cocheOff(id_objeto) {
+                document.getElementById(id_objeto).style.width="15%";
+            }
