6b64d0cf348507a6cb6a624ae290df058607c441
LRP <lufer5775@gmail.com>
2026-03-03 17:54:54 +0100

    con examen realizado

---
+++ Examen2evaluacion_LRP/Funciones/funciones.js
@@ -0,0 +1,41 @@
+
+function foco(id_objeto) {
+    document.getElementById(id_objeto).style.border = "6px solid red";
+}
+function sinfoco(id_objeto) {
+    document.getElementById(id_objeto).style.border = "2px solid black";
+}
+
+function focozona(id_objeto) {
+    document.getElementById(id_objeto).style.backgroundColor = "rgba(155, 155, 155, 0.5)";
+
+}
+function sinfocozona(id_objeto) {
+    document.getElementById(id_objeto).style.backgroundColor = "transparent";
+
+}
+function fecha(id_objeto) {
+     const hoy = new Date().toLocaleDateString(); 
+    alert("La fecha de hoy es: " + hoy); 
+}
+
+
+
+function confirmar(event) {
+
+    const confirmacion = confirm("¿Estás seguro? Vamos a enviar los datos.");
+
+    if (!confirmacion) {
+        event.preventDefault();
+    }
+}
+function anular(event) {
+
+    const confirmacion = confirm("¿No quieres aprobar?");
+
+    if (confirmacion) {
+        
+    } else {
+        event.preventDefault();
+    }
+}
